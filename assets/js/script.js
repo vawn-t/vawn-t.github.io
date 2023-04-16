de8c43804ba43bf9a26595d577cad5ae7d4dc3bf
vawn <vantran99dn@gmail.com>
2023-04-16 19:08:14 +0700

    Implement layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-const wrapper = document.querySelector('.wrapper');
+const wrapper = document.querySelector('.scroll-wrapper');
+const selected = document.querySelector('.box-center');
 
 /**
  * Handle horizontal scroll
@@ -8,6 +9,32 @@ const handleScroll = (event) => {
 
   // Scroll the page horizontally by the calculated amount
   wrapper.scrollBy(event.deltaY, 0);
+
+  const containerRect = wrapper.getBoundingClientRect();
+
+  // Calculate the center of the selected item
+  const selectedRect = selected.getBoundingClientRect();
+  const center =
+    selectedRect.left - containerRect.left + selectedRect.width / 2;
+  // updatePositionIndicator(center);
+
+  console.log(center);
 };
 
 wrapper.addEventListener('wheel', handleScroll);
+
+// const handleScroll = () => {
+//   const selected = document.querySelector('.selected');
+//   const wrapper = document.querySelector('.wrapper');
+//   const containerRect = wrapper.getBoundingClientRect();
+
+//   // Calculate the center of the selected item
+//   const selectedRect = selected.getBoundingClientRect();
+//   const center =
+//     selectedRect.left - containerRect.left + selectedRect.width / 2;
+
+//   // Do something with the center value, such as update a position indicator
+//   updatePositionIndicator(center);
+// };
+
+// wrapper.addEventListener('scroll', handleScroll);
