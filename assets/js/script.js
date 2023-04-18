b405e29bd7731ccc78eabbabfe539673184e2a31
vawn <vantran99dn@gmail.com>
2023-04-18 23:12:36 +0700

    Implement switch theme feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const wrapper = document.querySelector('.scroll-wrapper');
 const selected = document.querySelector('.box-center');
+const lightModeBtn = document.getElementById('light-mode');
+const darkModeBtn = document.getElementById('dark-mode');
 
 /**
  * Handle horizontal scroll
@@ -9,32 +11,51 @@ const handleScroll = (event) => {
 
   // Scroll the page horizontally by the calculated amount
   wrapper.scrollBy(event.deltaY, 0);
+};
 
-  const containerRect = wrapper.getBoundingClientRect();
+/**
+ * Handle switch theme
+ */
+const handleSwitchTheme = (event) => {
+  const btn = event.target.closest('a');
+  const icon = event.target.closest('img');
+  const activeClass = 'theme-active';
 
-  // Calculate the center of the selected item
-  const selectedRect = selected.getBoundingClientRect();
-  const center =
-    selectedRect.left - containerRect.left + selectedRect.width / 2;
-  // updatePositionIndicator(center);
+  if (!btn.classList.contains(activeClass)) {
+    console.log('here');
+    // Remove light-mode class from DOM
+    document.querySelector(`.${activeClass}`).classList.remove(activeClass);
+    document.body.className = '';
 
-  console.log(center);
+    // Active theme button
+    btn.classList.add(activeClass);
+
+    // Active theme background
+    document.body.className = '';
+    if (btn.getAttribute('id') === 'light-mode') {
+      document.body.className = 'light-theme';
+
+      // Update icon
+      const darkIcon = document.querySelector('#dark-mode > img');
+      icon.src = icon.getAttribute('src').replace('disabled', 'enabled');
+      darkIcon.src = darkIcon
+        .getAttribute('src')
+        .replace('enabled', 'disabled');
+    } else {
+      document.body.className = 'dark-theme';
+
+      // Update icon
+      const lightIcon = document.querySelector('#light-mode > img');
+      icon.src = icon.getAttribute('src').replace('disabled', 'enabled');
+      lightIcon.src = lightIcon
+        .getAttribute('src')
+        .replace('enabled', 'disabled');
+    }
+
+    // Update icons
+  }
 };
 
 wrapper.addEventListener('wheel', handleScroll);
-
-// const handleScroll = () => {
-//   const selected = document.querySelector('.selected');
-//   const wrapper = document.querySelector('.wrapper');
-//   const containerRect = wrapper.getBoundingClientRect();
-
-//   // Calculate the center of the selected item
-//   const selectedRect = selected.getBoundingClientRect();
-//   const center =
-//     selectedRect.left - containerRect.left + selectedRect.width / 2;
-
-//   // Do something with the center value, such as update a position indicator
-//   updatePositionIndicator(center);
-// };
-
-// wrapper.addEventListener('scroll', handleScroll);
+lightModeBtn.addEventListener('click', handleSwitchTheme);
+darkModeBtn.addEventListener('click', handleSwitchTheme);
